a1e6980f3233208a55ac55ed6c54e947564b6743
Dudeiebot <ojoexcel5@gmail.com>
2023-11-27 22:23:49 +0100

    main.go

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strings"
+	"unicode"
+)
+
 // duplicate num in a array
 
 // func DupNum(nums []int) bool {
@@ -27,23 +32,42 @@ package main
 
 // valid anagram
 // you can first of all do it by spliting(strings.split), sort it (sort.strings) and then compare/join with (strings.join) or
-func isAnagram(s, t string) bool {
-	// make our map
-	c_s := make(map[rune]int)
-	c_t := make(map[rune]int)
+// func isAnagram(s, t string) bool {
+// 	// make our map
+// 	c_s := make(map[rune]int)
+// 	c_t := make(map[rune]int)
+//
+// 	// add all the character individually to the map
+// 	for _, char := range s {
+// 		c_s[char]++
+// 	}
+//
+// 	for _, char := range t {
+// 		c_t[char]++
+// 	}
+//
+// 	// range through our mao and check if the count of c_s matches the one of c_t
+// 	for char, count := range c_s {
+// 		if count != c_t[char] {
+// 			return false
+// 		}
+// 	}
+// 	return true
+// }
 
-	// add all the character individually to the map
-	for _, char := range s {
-		c_s[char]++
-	}
+// valid Palindrome
+// before i wanted to use the rune datatype reverse model but it takes a lot of memory and it is not fast
+// so these is me comparing the first and the last and going through it in a single for loop statement
+func IsPalindrome(s string) bool {
+	s = strings.Map(func(r rune) rune {
+		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+			return -1
+		}
+		return unicode.ToLower(r)
+	}, s)
 
-	for _, char := range t {
-		c_t[char]++
-	}
-
-	// range through our mao and check if the count of c_s matches the one of c_t
-	for char, count := range c_s {
-		if count != c_t[char] {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
 			return false
 		}
 	}
