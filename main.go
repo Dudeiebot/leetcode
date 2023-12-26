1eb05ce032ef8d06a58fb4a2c91dd51988571a3f
Dudeiebot <ojoexcel5@gmail.com>
2023-12-26 20:17:03 +0100

    comment

--- main.go
+++ main.go
@@ -122,18 +122,17 @@ package main
 // }
 //
 // bruteforce techniques O(n2)
-
-func twoSums(nums []int, target int) []int {
-	arr := make([]int, 2)
-
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				arr[0] = i
-				arr[1] = j
-				return arr
-			}
-		}
-	}
-	return nil
-}
+// func twoSums(nums []int, target int) []int {
+// 	arr := make([]int, 2)
+//
+// 	for i := 0; i < len(nums); i++ {
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i]+nums[j] == target {
+// 				arr[0] = i
+// 				arr[1] = j
+// 				return arr
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
