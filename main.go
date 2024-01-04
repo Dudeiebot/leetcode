6863e84f2cb9ab63615fbcec0251ed09c30174f8
Dudeiebot <ojoexcel5@gmail.com>
2024-01-04 15:02:06 +0100

    merging two linked list

--- main.go
+++ main.go
@@ -253,8 +253,8 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 
 	for list1 != nil && list2 != nil {
 		if list1.Val < list2.Val {
-			currentNode.Next = list1
-			list1 = list1.Next
+			currentNode.Next = list1 // this point to the currentvalue in our tempNode
+			list1 = list1.Next       // this point to our NextValue
 		} else {
 			currentNode.Next = list2
 			list2 = list2.Next
@@ -262,6 +262,7 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 		currentNode = currentNode.Next
 	}
 
+	// and when any of them come to finish here, we add the remaining to currentNode(which also lead to our tempNode)
 	if list1 != nil {
 		currentNode.Next = list1
 	} else {
