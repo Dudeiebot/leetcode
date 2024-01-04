c5a1016f014d04f6cf87f0cbe851dc05fe394512
Dudeiebot <ojoexcel5@gmail.com>
2024-01-04 14:51:01 +0100

    merging two linked list

--- main.go
+++ main.go
@@ -241,3 +241,31 @@ package main
 // 	}
 // 	return len(stack) == 0
 // }
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	tempNode := &ListNode{}
+	currentNode := tempNode
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			currentNode.Next = list1
+			list1 = list1.Next
+		} else {
+			currentNode.Next = list2
+			list2 = list2.Next
+		}
+		currentNode = currentNode.Next
+	}
+
+	if list1 != nil {
+		currentNode.Next = list1
+	} else {
+		currentNode.Next = list2
+	}
+	return tempNode.Next
+}
