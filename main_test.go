ece3379ccf5bc3e38940f15d4962686ac7478145
Dudeiebot <ojoexcel5@gmail.com>
2024-01-05 15:37:54 +0100

    remove elements test

--- main_test.go
+++ main_test.go
@@ -243,3 +243,23 @@ func TestRemoveDuplicates(t *testing.T) {
 		})
 	}
 }
+
+func TestRemoveElements(t *testing.T) {
+	tests := []struct {
+		nums []int
+		val  int
+		want int
+	}{
+		{[]int{3, 1, 2, 3}, 3, 2},
+		{[]int{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5},
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v", tc.nums), func(t *testing.T) {
+			got := removeElements(tc.nums, tc.val)
+			if got != tc.want {
+				t.Fatalf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
