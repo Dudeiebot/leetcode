e28a707e1e6e81fbde12767171ff16be8b0b02e5
Dudeiebot <ojoexcel5@gmail.com>
2024-01-08 20:28:07 +0100

    commenting search insert out

--- main.go
+++ main.go
@@ -317,30 +317,31 @@ package main
 // 	// s: ad == sad
 // }
 
-func searchInsert(nums []int, target int) int {
-	// l := 0
-	// r := len(nums) - 1
-	//
-	// for l <= r {
-	// 	mid := (l + r) / 2
-	// 	if nums[mid] < target {
-	// 		l = mid + 1
-	// 	} else if nums[mid] > target {
-	// 		r = mid - 1
-	// 	} else {
-	// 		return mid
-	// 	}
-	// }
-	// return l
-	//Binary search using Olog(n)
-
-	for k := range nums {
-		if nums[k] == target {
-			return k
-		} else if nums[k] > target {
-			return k
-		}
-	}
-	return len(nums)
-	// linear Search
-}
+// func searchInsert(nums []int, target int) int {
+// 	// l := 0
+// 	// r := len(nums) - 1
+// 	//
+// 	// for l <= r {
+// 	// 	mid := (l + r) / 2
+// 	// 	if nums[mid] < target {
+// 	// 		l = mid + 1
+// 	// 	} else if nums[mid] > target {
+// 	// 		r = mid - 1
+// 	// 	} else {
+// 	// 		return mid
+// 	// 	}
+// 	// }
+// 	// return l
+// 	//Binary search using Olog(n)
+//
+// 	for k := range nums {
+// 		if nums[k] == target {
+// 			return k
+// 		} else if nums[k] > target {
+// 			return k
+// 		}
+// 	}
+// 	return len(nums)
+// 	// linear Search
+// }
+//
