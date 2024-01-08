270cc0edf0556680e0a93736a288e0ffea77d35f
Dudeiebot <ojoexcel5@gmail.com>
2024-01-08 20:42:10 +0100

    commenting out

--- main.go
+++ main.go
@@ -345,17 +345,17 @@ package main
 // 	// linear Search
 // }
 
-func lengthOfLastWord(s string) int {
-	l := 0
-	i := len(s) - 1
-
-	for i >= 0 && s[i] == ' ' {
-		i--
-	}
-
-	for i >= 0 && s[i] != ' ' {
-		l++
-		i--
-	}
-	return l
-}
+// func lengthOfLastWord(s string) int {
+// 	l := 0
+// 	i := len(s) - 1
+//
+// 	for i >= 0 && s[i] == ' ' {
+// 		i--
+// 	}
+//
+// 	for i >= 0 && s[i] != ' ' {
+// 		l++
+// 		i--
+// 	}
+// 	return l
+// }
