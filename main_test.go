0c74c60f2063f91f97ad3e214626e517a7b073e2
Dudeiebot <ojoexcel5@gmail.com>
2024-01-24 14:21:14 +0100

    Remove element in linked list test

--- main_test.go
+++ main_test.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-	//	"fmt"
-	//	"reflect"
 	"fmt"
+	util "leetCode/struct"
+	"reflect"
 	"testing"
 )
 
-//
-//
 // the test files
-// func TestDupNum(t *testing.T) {
-// 	tests := []struct {
-// 		nums []int
-// 		want bool
-// 	}{
-// 		{[]int{1, 2, 3, 4, 1, 4, 5, 2}, true},
-// 		{[]int{1, 2, 3, 4, 5, 6, 7}, false},
-// 		{[]int{2, 1, 3, 4, 3, 5, 5, 7}, true},
-// 	}
-// 	for _, tc := range tests {
-// 		t.Run(fmt.Sprintf("%v", tc.nums), func(t *testing.T) {
-// 			got := DupNum(tc.nums)
-// 			if got != tc.want {
-// 				t.Fatalf("DupNum() = %v; want %v", got, tc.want)
-// 			}
-// 		})
-// 	}
-// }
+func TestDupNum(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want bool
+	}{
+		{[]int{1, 2, 3, 4, 1, 4, 5, 2}, true},
+		{[]int{1, 2, 3, 4, 5, 6, 7}, false},
+		{[]int{2, 1, 3, 4, 3, 5, 5, 7}, true},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v", tc.nums), func(t *testing.T) {
+			got := DupNum(tc.nums)
+			if got != tc.want {
+				t.Fatalf("DupNum() = %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
 
 // func TestIsAnagram(t *testing.T) {
 // 	tests := []struct {
@@ -1146,6 +1144,37 @@ func TestIsHappy(t *testing.T) {
 			got := isHappy(tc.n)
 			if got != tc.want {
 				t.Fatalf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestRemoveLinkedList(t *testing.T) {
+	tests := []struct {
+		head     *util.ListNode
+		val      int
+		expected *util.ListNode
+	}{
+		{
+			head: &util.ListNode{
+				Val: 1,
+				Next: &util.ListNode{
+					Val: 4, Next: &util.ListNode{Val: 3, Next: &util.ListNode{Val: 4}},
+				},
+			},
+			val: 4,
+			expected: &util.ListNode{
+				Val:  1,
+				Next: &util.ListNode{Val: 3},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run("", func(t *testing.T) {
+			result := removeLinkedList(tc.head, tc.val)
+			if !reflect.DeepEqual(result, tc.expected) {
+				t.Errorf("Expected %v, got %v", tc.expected, result)
 			}
 		})
 	}
