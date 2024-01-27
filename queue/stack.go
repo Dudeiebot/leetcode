e9ce7d233cfc3ae5bde29d83f0ebd4367a9fed75
Dudeiebot <ojoexcel5@gmail.com>
2024-01-27 20:39:39 +0100

    queue.go

--- queue/stack.go
+++ queue/stack.go
@@ -4,33 +4,64 @@ type MyStack struct {
 	a []int
 }
 
-func Constructor() MyStack {
-	return MyStack{}
+// LIFO queue to stack
+// func Constructor() MyStack {
+// 	return MyStack{}
+// }
+//
+// func (this *MyStack) Push(x int) {
+// 	this.a = append(this.a, x)
+// 	for i := 0; i < len(this.a)-1; i++ {
+// 		this.a = append(this.a, this.a[0])
+// 		this.a = this.a[1:]
+// 	}
+// }
+//
+// func (this *MyStack) Pop() int {
+// 	val := this.a[0]
+// 	this.a = this.a[1:]
+// 	return val
+// }
+//
+// func (this *MyStack) Top() int {
+// 	if len(this.a) == 0 {
+// 		return 0
+// 	}
+// 	return this.a[0]
+// }
+//
+// func (this *MyStack) Empty() bool {
+// 	return len(this.a) == 0
+// }
+
+// Putting out seperately for easy access of all
+
+// FIFO stack to queue
+type MyQueue struct {
+	a []int
 }
 
-func (this *MyStack) Push(x int) {
+func Constructor() MyQueue {
+	return MyQueue{}
+}
+
+func (this *MyQueue) Push(x int) {
 	this.a = append(this.a, x)
-	for i := 0; i < len(this.a)-1; i++ {
-		this.a = append(this.a, this.a[0])
-		this.a = this.a[1:]
-	}
 }
 
-func (this *MyStack) Pop() int {
+func (this *MyQueue) Pop() int {
 	val := this.a[0]
 	this.a = this.a[1:]
 	return val
 }
 
-func (this *MyStack) Top() int {
+func (this *MyQueue) Peek() int {
 	if len(this.a) == 0 {
 		return 0
 	}
 	return this.a[0]
 }
 
-func (this *MyStack) Empty() bool {
+func (this *MyQueue) Empty() bool {
 	return len(this.a) == 0
 }
-
-// Putting out seperately for easy access of all
