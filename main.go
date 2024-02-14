6d4dfb20ed16c250531cc89bd770cea488066a14
Dudeiebot <ojoexcel5@gmail.com>
2024-02-14 21:32:58 +0100

    isPowerOfFour

--- main.go
+++ main.go
@@ -1081,6 +1081,17 @@ func isPowerOfThree(n int) bool {
 	return n == 1
 }
 
+func isPowerOfFour(n int) bool {
+	if n < 1 {
+		return false
+	}
+
+	for n%4 == 0 {
+		n /= 4
+	}
+	return n == 1
+}
+
 // func isLinkPalindrome(head *util.ListNode) bool {
 // 	slow := head
 // 	fast := head
