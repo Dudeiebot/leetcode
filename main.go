b0d142f1bcf49aa0a8d3d418bdacbddd58b458ef
Dudeiebot <ojoexcel5@gmail.com>
2024-02-20 19:50:19 +0100

    fizzbuzz

--- main.go
+++ main.go
@@ -1487,3 +1487,20 @@ func longestPalindrome(s string) int {
 	}
 	return ans
 }
+
+func fizzbuzz(n int) []string {
+	res := make([]string, 0)
+
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			res = append(res, "FizzBuzz")
+		} else if i%3 == 0 {
+			res = append(res, "Fizz")
+		} else if i%5 == 0 {
+			res = append(res, "Buzz")
+		} else {
+			res = append(res, fmt.Sprintf("%d", i))
+		}
+	}
+	return res
+}
