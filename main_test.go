a939324504c4e2a5b9e5df84de51fd853c2a1a27
Dudeiebot <ojoexcel5@gmail.com>
2024-02-29 22:51:59 +0100

    find disappeared num tes

--- main_test.go
+++ main_test.go
@@ -1940,3 +1940,21 @@ func TestFindAllDuplicate(t *testing.T) {
 		})
 	}
 }
+
+func TestFindDisappearedNumber(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want []int
+	}{
+		{[]int{4, 3, 2, 7, 8, 2, 3, 1}, []int{5, 6}},
+		{[]int{1, 1}, []int{2}},
+	}
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v", tc.nums), func(t *testing.T) {
+			got := findDisapperedNumber(tc.nums)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("got %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
