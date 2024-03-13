b15fc35e4e843b267cd2f385bac5fc61a0e43b67
Dudeiebot <ojoexcel5@gmail.com>
2024-03-13 21:41:01 +0100

    ArrayPairs test

--- main_test.go
+++ main_test.go
@@ -2337,3 +2337,21 @@ func TestReverseWords(t *testing.T) {
 		})
 	}
 }
+
+func TestArrayPairs(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want int
+	}{
+		{[]int{1, 4, 3, 2}, 4},
+		{[]int{6, 2, 6, 5, 1, 2}, 9},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.nums), func(t *testing.T) {
+			got := arrayPairSum(tt.nums)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
