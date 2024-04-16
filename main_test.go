f43f56afadbdecc9ee9b48dec37fbbc6556dee77
Dudeiebot <ojoexcel5@gmail.com>
2024-04-16 22:44:44 +0100

    TestMinCostClimbingStairs

--- main_test.go
+++ main_test.go
@@ -2753,3 +2753,21 @@ func TestNextGreatestLetters(t *testing.T) {
 		})
 	}
 }
+
+func TestMinCostClimbingStairs(t *testing.T) {
+	tests := []struct {
+		cost []int
+		want int
+	}{
+		{[]int{10, 15, 20}, 15},
+		{[]int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.cost), func(t *testing.T) {
+			got := minCostClimbingStairs(tt.cost)
+			if got != tt.want {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
