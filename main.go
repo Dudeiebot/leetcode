30b46f0ced03f1e9341ac64f51af5f34e6791fdb
Dudeiebot <ojoexcel5@gmail.com>
2024-05-02 22:12:38 +0100

    LemonadeChange

--- main.go
+++ main.go
@@ -2950,3 +2950,35 @@ func backspaceCompare(s string, t string) bool {
 	}
 	return true
 }
+
+func lemonadeChange(bills []int) bool {
+	five := 0
+	ten := 0
+	twenty := 0
+
+	for _, bill := range bills {
+		switch bill {
+		case 5:
+			five++
+		case 10:
+			if five >= 1 {
+				ten++
+				five--
+			} else {
+				return false
+			}
+		case 20:
+			if five >= 1 && ten >= 1 {
+				twenty++
+				five--
+				ten--
+			} else if five >= 3 {
+				twenty++
+				five -= 3
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
