f5114fe2a497b21082f73e1e26109c5e8bbb1d45
Dudeiebot <ojoexcel5@gmail.com>
2024-05-02 22:37:30 +0100

    TestTransposeMatrix

--- main_test.go
+++ main_test.go
@@ -3116,3 +3116,21 @@ func TestLemonadeChange(t *testing.T) {
 		})
 	}
 }
+
+func TestTransposeMatrix(t *testing.T) {
+	tests := []struct {
+		matrix [][]int
+		want   [][]int
+	}{
+		{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, [][]int{{1, 4, 7}, {2, 5, 8}, {3, 6, 9}}},
+		{[][]int{{1, 2, 3}, {4, 5, 6}}, [][]int{{1, 4}, {2, 5}, {3, 6}}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.matrix), func(t *testing.T) {
+			got := transposeMatrix(tt.matrix)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
