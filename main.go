23b729933bb178e75bb6af0d9998aa3ed03256c1
Dudeiebot <ojoexcel5@gmail.com>
2024-05-14 23:03:12 +0100

    IsMonotonic

--- main.go
+++ main.go
@@ -3142,3 +3142,20 @@ func fairCandySwap(aliceSizes []int, bobSizes []int) []int {
 	}
 	return []int{}
 }
+
+func isMonotonic(nums []int) bool {
+	increasing := true
+	decreasing := true
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[i-1] {
+			decreasing = false
+		} else if nums[i] < nums[i-1] {
+			increasing = false
+		}
+		if !increasing && !decreasing {
+			return false
+		}
+	}
+	return true
+}
