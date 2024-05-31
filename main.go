a48f310ce8f386ecca615cbdf784fdce724041c0
Dudeiebot <ojoexcel5@gmail.com>
2024-05-31 22:20:22 +0100

    TestLargestPerimeter

--- main.go
+++ main.go
@@ -3346,3 +3346,14 @@ func repeatedNtimes(nums []int) int {
 	}
 	return -1
 }
+
+func largestPerimeter(nums []int) int {
+	sort.Ints(nums)
+
+	for i := len(nums) - 3; i >= 0; i-- {
+		if nums[i]+nums[i+1] > nums[i+2] {
+			return nums[i] + nums[i+1] + nums[i+2]
+		}
+	}
+	return 0
+}
