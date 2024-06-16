40446f5faf291c75a57cf36c65b2f455dd49c3c6
Dudeiebot <ojoexcel5@gmail.com>
2024-06-16 13:24:35 +0100

    CanThreePartsEqualSum

--- main.go
+++ main.go
@@ -3512,3 +3512,26 @@ func commonChars(words []string) []string {
 	}
 	return res
 }
+
+func canThreePartsEqualSum(arr []int) bool {
+	sum := 0
+
+	for _, num := range arr {
+		sum += num
+	}
+
+	if sum%3 != 0 {
+		return false
+	}
+
+	partSum, currentSum, count := sum/3, 0, 0
+
+	for i := 0; i < len(arr); i++ {
+		currentSum += arr[i]
+		if currentSum == partSum {
+			currentSum = 0
+			count++
+		}
+	}
+	return count >= 3
+}
