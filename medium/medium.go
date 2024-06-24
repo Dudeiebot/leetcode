70b463c60bcd41e161a7341ffed95db4e75b7fe0
Dudeiebot <ojoexcel5@gmail.com>
2024-06-24 23:22:25 +0100

    GroupAnagram

--- medium/medium.go
+++ medium/medium.go
@@ -1,1 +1,18 @@
 package medium
+
+func GroupAnagram(strs []string) [][]string {
+	hmap := map[[26]int][]string{}
+	res := [][]string{}
+
+	for _, word := range strs {
+		k := [26]int{}
+		for _, char := range word {
+			k[char-'a']++
+		}
+		hmap[k] = append(hmap[k], word)
+	}
+	for _, val := range hmap {
+		res = append(res, val)
+	}
+	return res
+}
