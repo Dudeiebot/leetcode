23d55401a607ee399f1cfd2a2dfac7d9f9c2f228
Dudeiebot <ojoexcel5@gmail.com>
2024-06-24 23:21:56 +0100

    TestGroupAnagram

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -1,1 +1,29 @@
 package medium
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestGroupAnagram(t *testing.T) {
+	tests := []struct {
+		strs []string
+		want [][]string
+	}{
+		{
+			[]string{"eat", "tea", "tan", "ate", "nat", "bat"},
+			[][]string{{"eat", "tea", "ate"}, {"tan", "nat"}, {"bat"}},
+		},
+		{[]string{""}, [][]string{{""}}},
+		{[]string{"a"}, [][]string{{"a"}}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.strs), func(t *testing.T) {
+			got := GroupAnagram(tt.strs)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
