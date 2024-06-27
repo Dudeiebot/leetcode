e7ce8c2e2eb774991dfd5d9090bfc9f520299082
Dudeiebot <ojoexcel5@gmail.com>
2024-06-27 20:25:32 +0100

    TestTopKFrequent

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -27,3 +27,22 @@ func TestGroupAnagram(t *testing.T) {
 		})
 	}
 }
+
+func TestTopKFrequent(t *testing.T) {
+	tests := []struct {
+		nums []int
+		k    int
+		want []int
+	}{
+		{[]int{1, 1, 1, 2, 2, 3}, 2, []int{1, 2}},
+		{[]int{1}, 1, []int{1}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v, %v", tt.nums, tt.k), func(t *testing.T) {
+			got := topKFrequent(tt.nums, tt.k)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
