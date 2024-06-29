567dddd066dd8d760c6677c35c8e82d582972ffb
Dudeiebot <ojoexcel5@gmail.com>
2024-06-29 23:17:50 +0100

    ProductExceptSelf

--- medium/medium.go
+++ medium/medium.go
@@ -49,3 +49,20 @@ func topKFrequent(nums []int, k int) []int {
 	}
 	return res
 }
+
+func productExceptSelf(nums []int) []int {
+	res := make([]int, len(nums))
+
+	prefix := 1
+	for i := 0; i < len(nums); i++ {
+		res[i] = prefix
+		prefix *= nums[i]
+	}
+
+	suffix := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		res[i] *= suffix
+		suffix *= nums[i]
+	}
+	return res
+}
