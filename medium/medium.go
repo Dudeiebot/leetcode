035cd5cadea2e00c95b69cfe3be1cb56dc51f4a0
Dudeiebot <ojoexcel5@gmail.com>
2024-07-04 21:56:57 +0100

    TwoSumII

--- medium/medium.go
+++ medium/medium.go
@@ -92,3 +92,18 @@ func longestConsecutive(nums []int) int {
 	}
 	return maxLen
 }
+
+func twoSumII(nums []int, target int) []int {
+	l, r := 0, len(nums)-1
+
+	for l < r {
+		if nums[l]+nums[r] > target {
+			r--
+		} else if nums[l]+nums[r] < target {
+			l++
+		} else {
+			return []int{l + 1, r + 1}
+		}
+	}
+	return nil
+}
