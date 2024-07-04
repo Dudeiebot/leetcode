3626faa69c4da3754e9e452a539cbfe7fa46a049
Dudeiebot <ojoexcel5@gmail.com>
2024-07-04 21:56:51 +0100

    TestTwoSumII

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -82,3 +82,23 @@ func TestLongestConsecutive(t *testing.T) {
 		})
 	}
 }
+
+func TestTwoSumII(t *testing.T) {
+	tests := []struct {
+		nums   []int
+		target int
+		want   []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{1, 2}},
+		{[]int{2, 3, 4}, 6, []int{1, 3}},
+		{[]int{-1, 0}, -1, []int{1, 2}},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v, %v", tt.nums, tt.target), func(t *testing.T) {
+			got := twoSumII(tt.nums, tt.target)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
