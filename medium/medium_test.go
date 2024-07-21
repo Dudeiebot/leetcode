4031e6a73628dfd20c4f041e149c196567288a10
Dudeiebot <ojoexcel5@gmail.com>
2024-07-21 21:06:46 +0100

    FindMIn

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -158,3 +158,22 @@ func TestLengthOfLongestSubstring(t *testing.T) {
 		})
 	}
 }
+
+func TestFindMIn(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want int
+	}{
+		{[]int{3, 4, 5, 1, 2}, 1},
+		{[]int{4, 5, 6, 7, 0, 1, 2}, 0},
+		{[]int{11, 13, 15, 17}, 11},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.nums), func(t *testing.T) {
+			got := findMin(tt.nums)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("%v, %v", got, tt.want)
+			}
+		})
+	}
+}
