4fc66edeade2bba378daeb23df2a1450dc906965
Dudeiebot <ojoexcel5@gmail.com>
2024-07-22 20:55:46 +0100

    Search from a sorted array

--- medium/medium.go
+++ medium/medium.go
@@ -179,8 +179,34 @@ func findMin(nums []int) int {
 		if nums[l] > nums[r] {
 			l = m + 1
 		} else {
-			m = r
+			r = m
 		}
 	}
 	return nums[l]
+}
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		m := l + (r-l)/2
+		if nums[m] == target {
+			return m
+		}
+
+		if nums[l] <= nums[m] {
+			if target >= nums[l] && target < nums[m] {
+				r = m - 1
+			} else {
+				l = m + 1
+			}
+		} else {
+			if target > nums[m] && target <= nums[r] {
+				l = m + 1
+			} else {
+				r = m - 1
+			}
+		}
+	}
+	return -1
 }
