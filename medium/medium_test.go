20ef510fe79c675f12c566892d300b86acecfa43
Dudeiebot <ojoexcel5@gmail.com>
2024-07-28 22:10:18 +0100

    TestCharacterReplacement

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -159,6 +159,25 @@ func TestLengthOfLongestSubstring(t *testing.T) {
 	}
 }
 
+func TestCharacterReplacement(t *testing.T) {
+	tests := []struct {
+		s    string
+		k    int
+		want int
+	}{
+		{"ABAB", 2, 4},
+		{"AABABBA", 1, 4},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v, %v", tt.s, tt.k), func(t *testing.T) {
+			got := characterReplacement(tt.s, tt.k)
+			if got != tt.want {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestFindMIn(t *testing.T) {
 	tests := []struct {
 		nums []int
