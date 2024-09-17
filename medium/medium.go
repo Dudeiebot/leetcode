6492f23f65faf50a09f5599c6ff96c049334d331
Dudeiebot <ojoexcel5@gmail.com>
2024-09-17 21:54:33 +0100

    longest palindrome

--- medium/medium.go
+++ medium/medium.go
@@ -569,3 +569,29 @@ func robII(nums []int) int {
 	}
 	return max(helper(nums[1:]), helper(nums[:len(nums)-1]))
 }
+
+func longestPalindrome(s string) string {
+	res := ""
+
+	for i := 0; i < len(s); i++ {
+		// odd length palindrome
+		l, r := i, i
+		for l >= 0 && r < len(s) && s[l] == s[r] {
+			if (r - l + 1) > len(res) {
+				res = s[l : r+1]
+			}
+			l--
+			r++
+		}
+		l, r = i, i+1
+		// even length palindrome
+		for l >= 0 && r < len(s) && s[l] == s[r] {
+			if (r - l + 1) > len(res) {
+				res = s[l : r+1]
+			}
+			l--
+			r++
+		}
+	}
+	return res
+}
