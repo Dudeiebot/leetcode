a4a6b47b25b98e15bae169629c927b047f97cea3
Dudeiebot <ojoexcel5@gmail.com>
2024-09-25 16:13:58 +0100

    TestLengthOfLIS

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -460,3 +460,22 @@ func TestWordBreak(t *testing.T) {
 		})
 	}
 }
+
+func TestLengthOfLIS(t *testing.T) {
+	test := []struct {
+		nums []int
+		want int
+	}{
+		{[]int{10, 9, 2, 5, 3, 7, 101, 18}, 4},
+		{[]int{0, 1, 0, 3, 2, 3}, 4},
+		{[]int{7, 7, 7, 7, 7, 7, 7}, 1},
+	}
+	for _, tt := range test {
+		t.Run(fmt.Sprintf("%v", tt.nums), func(t *testing.T) {
+			got := lengthOfLIS(tt.nums)
+			if got != tt.want {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
