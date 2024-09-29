ad6fa8ffc989062ca5ca657eca2fe33b4dbe5f38
Dudeiebot <ojoexcel5@gmail.com>
2024-09-29 23:44:00 +0100

    LongestCommonSubsequence

--- medium/medium.go
+++ medium/medium.go
@@ -716,3 +716,21 @@ func uniquePathsDp(m int, n int) int {
 	}
 	return dp[m-1][n-1]
 }
+
+func longestCommonSubsequence(text1, text2 string) int {
+	dp := [][]int{}
+
+	for i := 0; i < len(text1)+1; i++ {
+		dp = append(dp, make([]int, len(text2)+1))
+	}
+	for i := 1; i < len(text1)+1; i++ {
+		for j := 1; j < len(text2)+1; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[len(text1)][len(text2)]
+}
