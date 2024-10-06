a9307b981dfc24bd2e3f9555cb8d978b389b7a95
Dudeiebot <ojoexcel5@gmail.com>
2024-10-06 23:03:17 +0100

    NumDecoding

--- medium/medium.go
+++ medium/medium.go
@@ -618,6 +618,31 @@ func palindromicSubstring(s string) int {
 	return res
 }
 
+func numDecodings(s string) int {
+	if len(s) == 0 || s[0] == '0' {
+		return 0
+	}
+
+	n := len(s)
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+
+	for i := 2; i <= n; i++ {
+		// Check one-digit number
+		if s[i-1] != '0' {
+			dp[i] += dp[i-1]
+		}
+
+		// Check two-digit number
+		twoDigit := s[i-2 : i]
+		if twoDigit >= "10" && twoDigit <= "26" {
+			dp[i] += dp[i-2]
+		}
+	}
+
+	return dp[n]
+}
+
 func wordBreak(s string, wordDict []string) bool {
 	q := []string{s}
 	memo := make(map[string]bool)
