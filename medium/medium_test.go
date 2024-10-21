a75e5823ac7d4a41a7087a9807911b13160f48cd
Dudeiebot <ojoexcel5@gmail.com>
2024-10-21 18:31:35 +0100

    Test Rotating an imgar 90 degree clockwise

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -671,3 +671,24 @@ func TestMerge(t *testing.T) {
 		})
 	}
 }
+
+func TestRotate(t *testing.T) {
+	tests := []struct {
+		matrix [][]int
+		want   [][]int
+	}{
+		{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, [][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}}},
+		{
+			[][]int{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 1, 16}},
+			[][]int{{15, 13, 2, 5}, {14, 3, 4, 1}, {12, 6, 8, 9}, {16, 7, 10, 11}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.matrix), func(t *testing.T) {
+			rotate(tt.matrix)
+			if !reflect.DeepEqual(tt.matrix, tt.want) {
+				t.Fatalf("got %v, want %v", tt.matrix, tt.want)
+			}
+		})
+	}
+}
