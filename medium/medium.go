edb7a89fc86a1ce750a0df46f684ec16589bdf9a
Dudeiebot <ojoexcel5@gmail.com>
2024-11-04 20:47:12 +0100

    spiralOrder

--- medium/medium.go
+++ medium/medium.go
@@ -919,6 +919,51 @@ func rotate(matrix [][]int) {
 	}
 }
 
+func spiralOrder(matrix [][]int) []int {
+	if matrix == nil || len(matrix) == 0 {
+		return []int{}
+	}
+	res := []int{}
+	top, bottom, left, right := 0, len(matrix)-1, 0, len(matrix[0])-1
+	for {
+		// right
+		for i := left; i <= right; i++ {
+			res = append(res, matrix[top][i])
+		}
+		top++
+		if top > bottom {
+			break
+		}
+		// down
+		for i := top; i <= bottom; i++ {
+			res = append(res, matrix[i][right])
+		}
+		right--
+		if left > right {
+			break
+		}
+
+		// left
+		for i := right; i >= left; i-- {
+			res = append(res, matrix[bottom][i])
+		}
+		bottom--
+		if top > bottom {
+			break
+		}
+
+		// up
+		for i := bottom; i >= top; i-- {
+			res = append(res, matrix[i][left])
+		}
+		left++
+		if left > right {
+			break
+		}
+	}
+	return res
+}
+
 func setZeroes(matrix [][]int) {
 	n := len(matrix)
 	m := len(matrix[0])
