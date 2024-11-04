c70e1939a834e7d4fd79d0096a63cdd5c7b63f64
Dudeiebot <ojoexcel5@gmail.com>
2024-11-04 20:47:23 +0100

    TestSpiralOrder

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -712,6 +712,27 @@ func TestRotate(t *testing.T) {
 	}
 }
 
+func TestSpiralOrder(t *testing.T) {
+	tests := []struct {
+		matrix [][]int
+		want   []int
+	}{
+		{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, []int{1, 2, 3, 6, 9, 8, 7, 4, 5}},
+		{
+			[][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}},
+			[]int{1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.matrix), func(t *testing.T) {
+			got := spiralOrder(tt.matrix)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestSetZeroes(t *testing.T) {
 	tests := []struct {
 		matrix [][]int
