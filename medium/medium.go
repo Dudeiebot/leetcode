4d2cbc51ddd37dfa9ff463fcced1aef8ab5b78c7
Dudeiebot <ojoexcel5@gmail.com>
2024-11-24 14:59:45 +0100

    IsValidSudoku

--- medium/medium.go
+++ medium/medium.go
@@ -1086,3 +1086,19 @@ func generateParenthesis(n int) []string {
 	backtrack("", 0, 0)
 	return res
 }
+
+func isValidSudoku(board [][]byte) bool {
+	var rows, columns, squares [9][9]bool
+	for i, row := range board {
+		for j, v := range row {
+			if v != '.' {
+				k := int(v) - 49
+				if rows[i][k] || columns[j][k] || squares[i/3*3+j/3][k] {
+					return false
+				}
+				rows[i][k], columns[j][k], squares[i/3*3+j/3][k] = true, true, true
+			}
+		}
+	}
+	return true
+}
