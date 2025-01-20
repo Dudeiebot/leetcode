2baf2ee5e70ca8206e85c92f9222321c12688008
Dudeiebot <ojoexcel5@gmail.com>
2025-01-20 17:29:30 +0100

    reverse K Group

--- medium/medium.go
+++ medium/medium.go
@@ -1222,3 +1222,25 @@ func mergeKList(lists []*util.ListNode) *util.ListNode {
 	}
 	return lists[0]
 }
+
+func reverseKGroup(head *util.ListNode, k int) *util.ListNode {
+	node, count := head, 0
+
+	for count < k {
+		if node == nil {
+			return head
+		}
+		node = node.Next
+		count++
+	}
+	prev := reverseKGroup(node, k)
+
+	for count > 0 {
+		next := head.Next
+		head.Next = prev
+		prev = head
+		head = next
+		count--
+	}
+	return prev
+}
