a16ebd32310f78650d891c29d7b2872f3c039f5d
Dudeiebot <ojoexcel5@gmail.com>
2025-01-20 15:43:02 +0100

    finding duplicates (a linked list scenario)

--- medium/medium.go
+++ medium/medium.go
@@ -1185,3 +1185,24 @@ func addTwoNumbers(l1, l2 *util.ListNode) *util.ListNode {
 	}
 	return temp.Next
 }
+
+func FindsDuplicates(nums []int) int {
+	slow := nums[0]
+	fast := nums[0]
+
+	for {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+
+		if slow == fast {
+			break
+		}
+	}
+
+	slow = nums[0]
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+	return slow
+}
