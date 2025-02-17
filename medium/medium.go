30ceeef4389e1363f025363a2267365977798809
Dudeiebot <ojoexcel5@gmail.com>
2025-02-18 00:38:54 +0100

    exists

--- medium/medium.go
+++ medium/medium.go
@@ -1434,3 +1434,37 @@ func subsetsII(nums []int) [][]int {
 	dfs(0, nil)
 	return res
 }
+
+func exist(board [][]byte, word string) bool {
+	var dfs func(r, c, i int) bool
+
+	dfs = func(r, c, i int) bool {
+		if r < 0 || r > len(board)-1 || c < 0 || c > len(board[0])-1 || board[r][c] == '0' {
+			return false
+		}
+
+		if board[r][c] != word[i] {
+			return false
+		}
+
+		if i == len(word)-1 {
+			return word[i] == board[r][c]
+		}
+
+		val := board[r][c]
+		board[r][c] = '0'
+		if dfs(r, c-1, i+1) || dfs(r, c+1, i+1) || dfs(r-1, c, i+1) || dfs(r+1, c, i+1) {
+			return true
+		}
+		board[r][c] = val
+		return false
+	}
+	for r := 0; r < len(board); r++ {
+		for c := 0; c < len(board[0]); c++ {
+			if dfs(r, c, 0) {
+				return true
+			}
+		}
+	}
+	return false
+}
