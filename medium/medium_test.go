800d2448699689e80cf98a26ea86d043b4ea2606
Dudeiebot <ojoexcel5@gmail.com>
2026-09-13 15:06:25 +0100

    feat: can jum, min distance and min path sum test

--- medium/medium_test.go
+++ medium/medium_test.go
@@ -1047,3 +1047,71 @@ func TestExist(t *testing.T) {
 		})
 	}
 }
+
+func TestCanJumpII(t *testing.T) {
+	tests := []struct {
+		nums []int
+		want int
+	}{
+		{[]int{2, 3, 1, 1, 4}, 2},
+		{[]int{2, 3, 0, 1, 4}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.nums), func(t *testing.T) {
+			got := canJumpII(tt.nums)
+			if got != tt.want {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMinPathSum(t *testing.T) {
+	tests := []struct {
+		grid [][]int
+		want int
+	}{
+		{
+			[][]int{
+				{1, 3, 1},
+				{1, 5, 1},
+				{4, 2, 1},
+			},
+			7,
+		},
+		{
+			[][]int{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9},
+			},
+			21,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.grid), func(t *testing.T) {
+			got := minPathSum(tt.grid)
+			if got != tt.want {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUniquePathsII(t *testing.T) {
+	tests := []struct {
+		grid [][]int
+		want int
+	}{
+		{[][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}, 2},
+		{[][]int{{0, 1}, {0, 0}}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.grid), func(t *testing.T) {
+			got := uniquePathsII(tt.grid)
+			if got != tt.want {
+				t.Fatalf("got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
